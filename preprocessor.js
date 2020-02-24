ce3b991aa5516724ca47157c9884c136838dbbc5
Jonathan Edwards <apples_are_great@hotmail.co.uk>
2020-02-24 23:55:17 +0000

    check for define parameters

--- preprocessor.js
+++ preprocessor.js
@@ -1,12 +1,20 @@
 const PreProcessor = function(file){
 
+    const defineList = [];
+    const warnings = [];
+    const errors = [];
 
     function process(){
         let word = "";
         let processorFound = false;
         let processorStart;
         let processorEnd;
+        let lineCount = 1;
         for(let i = 0; i < file.length; i++){
+
+            if (file[i] === "\n") {
+                lineCount++;
+            }
             
             if (file[i] === "#") {//and not in comment or string
                 if(processorFound){
@@ -24,7 +32,7 @@ const PreProcessor = function(file){
                 if(file[i] === "\n"){
                     console.log(word);
                     processorEnd = i;
-                    lookAtProcessor(word, processorStart, processorEnd);
+                    lookAtProcessor(word, processorStart, processorEnd, lineCount);
                     processorFound = false;
                 }
 
@@ -37,11 +45,13 @@ const PreProcessor = function(file){
             
 
         }
+        console.log("Warnings: " + warnings);
+        console.log("Errors: " + errors);
 
         return "done";
     }
 
-    function lookAtProcessor(processor, start, end){
+    function lookAtProcessor(processor, start, end, lineCount){
         let tkns = processor.split(" ");
         console.log(tkns);
         if(tkns[0] === "define"){
@@ -49,13 +59,55 @@ const PreProcessor = function(file){
                 identifier: "",
                 replacement: "",
                 function: false,
+                inputs: [],
                 start: start,
                 end: end,
             };
             if(tkns.length > 1){
-                //test for defines with same name
-                //test is identifier is function
-                newDefine.identifier = tkns[1];
+                let inFunction = false;
+                let indentifer = "";
+                let input = "";
+                let name = tkns.slice(1, tkns.length).join(" ");
+
+                //test to see if identifer takes parameters
+                for (let i = 0; i < name.length; i++){
+                    if(inFunction){
+                        if (name[i] === ")"){
+                            if (input.trim() === "") {
+                                errors.push("line " + lineCount + ": invalid define parameter");
+                                return;
+                            } else {
+                                newDefine.inputs.push(input.trim());
+                                input = "";
+                            }
+                            //make sure we look at rest of definition as inden(a,b)a will all be 1 token but we a will be part of replacement
+                            break;
+                        }
+                        else if (name[i] === ","){
+                            
+                            if(input.trim() === ""){
+                                errors.push("line " + lineCount + ": invalid define parameter");
+                                return;
+                            }else{
+                                console.log("new");
+                                newDefine.inputs.push(input.trim());
+                                input = "";
+                                continue;
+                            }
+                        }
+                        input += name[i];
+                    }
+                    else if (name[i] === "("){
+                        newDefine.function = true;
+                        inFunction = true;
+                        continue;
+                    }else{
+                        //check if space. if is then we move onto looking at replacement
+                        indentifer += name[i];
+                    }
+                }
+
+                newDefine.identifier = indentifer;
 
                 if(tkns.length > 2){
                     let replacement = "";
@@ -66,14 +118,28 @@ const PreProcessor = function(file){
                             replacement += tkns[i] + " ";
                         }
                     }
+
+                    //test to remove newlines
                     newDefine.replacement = replacement;
                 }
+
+                //check if we already have define with same name
+                const found = defineList.findIndex((elm) => elm.identifier == newDefine.identifier);
+
+                if(found === -1){ 
+                    defineList.push(newDefine);
+                }else{
+                    warnings.push("line " + lineCount + ": redefintion of " + newDefine.identifier);
+                    defineList[found] = newDefine;
+                }
                
-                console.log(newDefine);
+                console.log(defineList);
+                
                 
 
             }else{
-                console.log("define without name");
+                errors.push("line " + lineCount + ": #define without name");
+                return;
             }
         }
     }

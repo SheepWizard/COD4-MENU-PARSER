579acb305fd8ebfa1348c370610e59ab71f63db2
Jonathan Edwards <apples_are_great@hotmail.co.uk>
2020-06-06 02:21:58 +0100

    Itemdef formatting

--- modules/Editor.js
+++ modules/Editor.js
@@ -4,9 +4,9 @@ export let editor;
 export function setEditor(e){
     editor = e;
 
-    const itemDef1 = 'itemDef{ \n\t\t\tname \"Item_1\"\n\t\t\trect (10*2) (100/(2.2*2)) 100 100\n\t\t\tvisible 1\n\t\t\tstyle 1\n\t\t\tbackColor 0 1 0 1\n\t\t}'
-    const itemDef2 = 'itemDef{ \n\t\t\tname \"Item_2\"\n\t\t\trect 0 0 100 100 2 2\n\t\t\tvisible 1\n\t\t\tstyle 1\n\t\t\tbackColor 1 0.7 0.3 1\n\t\t\tborder 1\n\t\t\tborderSize 4\n\t\t\tborderColor 0.5 0.2 0.1 1}'
-    const itemDef3 = 'itemDef{ \n\t\t\tname \"Item_3\"\n\t\t\trect 60 60 200 200 0 0\n\t\t\tvisible 1\n\t\t\tstyle 1\n\t\t\tbackColor 1 0 1 0.5\n\t\t\tborder 5\n\t\t\tborderSize 8\n\t\t\tborderColor (51/255) (204/255) (204/255) 1}'
+    const itemDef1 = 'itemDef{ \n\t\t\tname \"Item_1\"\n\t\t\trect (10*2) (100/(2.2*2)) 100 100\n\t\t\tvisible 1\n\t\t\tstyle 1\n\t\t\tbackColor 0 1 0 1\n\t}'
+    const itemDef2 = 'itemDef{ \n\t\t\tname \"Item_2\"\n\t\t\trect 0 0 100 100 2 2\n\t\t\tvisible 1\n\t\t\tstyle 1\n\t\t\tbackColor 1 0.7 0.3 1\n\t\t\tborder 1\n\t\t\tborderSize 4\n\t\t\tborderColor 0.5 0.2 0.1 1\n\t}'
+    const itemDef3 = 'itemDef{ \n\t\t\tname \"Item_3\"\n\t\t\trect 60 60 200 200 0 0\n\t\t\tvisible 1\n\t\t\tstyle 1\n\t\t\tbackColor 1 0 1 0.5\n\t\t\tborder 5\n\t\t\tborderSize 8\n\t\t\tborderColor (51/255) (204/255) (204/255) 1\n\t}'
 
     editor.setValue(`{ \n\tmenuDef{\n\t\tname \"Menu_1\"\n\t\trect 0 0 (600+40) 480\n\t\tborder 1\n\t\tborderSize 4\n\t\tvisible 1\n\t\tborderColor 0.25 0 0.5 1\n\t\n\n\t${itemDef1}\n\t${itemDef2}\n\t${itemDef3}\n\t\t}\n}`)
 }
